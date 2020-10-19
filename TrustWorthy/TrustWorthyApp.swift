e9038d119d50cb9125ea183482d120141c35d680
Dr-Bluemond <dr_bluemond@126.com>
2020-10-19 16:30:46 +0800

    Initial Commit

---
+++ TrustWorthy/TrustWorthyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrustWorthyApp.swift
+//  TrustWorthy
+//
+//  Created by bluemond on 2020/10/19.
+//
+
+import SwiftUI
+
+@main
+struct TrustWorthyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
